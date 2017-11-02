ee0d49fe62ad2afbb634f7b1944c8b6c5104434d
pabloFuente <pablofuenteperez@gmail.com>
2017-11-02 11:31:16 +0100

    Initial commit

---
+++ src/test/java/io/openvidu/test/e2e/browser/ChromeUser.java
@@ -0,0 +1,52 @@
+/*
+ * (C) Copyright 2017 OpenVidu (http://openvidu.io/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.openvidu.test.e2e.browser;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.core.io.ClassPathResource;
+
+public class ChromeUser extends BrowserUser {
+
+	public ChromeUser(String userName, int timeOfWaitInSeconds) {
+		super(userName, timeOfWaitInSeconds);
+
+		ChromeOptions options = new ChromeOptions();
+		// This flag avoids to grant the user media
+		options.addArguments("--use-fake-ui-for-media-stream");
+		// This flag fakes user media with synthetic video
+		options.addArguments("--use-fake-device-for-media-stream");
+		// This flag selects the entire screen as video source when screen sharing
+		options.addArguments("--auto-select-desktop-capture-source=Entire screen");
+		
+		try {
+			// Add Screen Sharing extension
+			options.addExtensions(new ClassPathResource("ScreenCapturing.crx").getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		this.driver = new ChromeDriver(options);
+		this.driver.manage().timeouts().setScriptTimeout(this.timeOfWaitInSeconds, TimeUnit.SECONDS);
+		
+		this.configureDriver();
+	}
+
+}
